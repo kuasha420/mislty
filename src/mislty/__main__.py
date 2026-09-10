77a001156ca5fd05215581b6dd29d69610077b67
Arafat Zahan <kuasha420@gmail.com>
2026-09-10 11:08:03 +0600

    chore(cli): add package __main__.py entrypoints for python -m mislty

---
+++ src/mislty/__main__.py
@@ -0,0 +1,6 @@
+"""MisLTy package executable entrypoint."""
+import sys
+from mislty.cli.main import main
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ src/mislty/cli/__main__.py
@@ -0,0 +1,6 @@
+"""MisLTy CLI package executable entrypoint."""
+import sys
+from mislty.cli.main import main
+
+if __name__ == "__main__":
+    sys.exit(main())
